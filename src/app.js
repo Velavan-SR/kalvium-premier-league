73e586837f01f0317b1642870503c8bf4cb6156a
Velavan Sundharamoorthy <velavan.sundharamoorth@kalvium.community>
2023-08-22 19:55:07 +0530

    half done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ let trophiesWon = 27;
 
 //Write your function here
 
+function createManager(name,age,team,trophies){
+    var managerDetails =[name,age,team,trophies];
+    return managerDetails
+};
+
 // Don't edit the following code
 try {
   var manager = createManager(
@@ -23,6 +28,15 @@ var formation = [4, 4, 3];
 
 //write your function here
 
+function createFormation(){
+  var formationDetails = {
+    defender : 4,
+    midfield : 4,
+    forward : 3
+  }
+  return formationDetails
+};
+
 // Dont edit the following code
 
 try {
@@ -33,17 +47,73 @@ try {
 
 //Progression 3 - Filter players that debuted in ___ year
 
+function filterByDebut(year){
+  var yearFilter = players.filter((player)=>{
+    return player.debut===year;
+  });
+  return yearFilter;
+};
+
 //Progression 4 - Filter players that play at the position _______
+
+function filterByPosition(position){
+  var positionFilter = players.filter((player) =>{
+    return player.position===position;
+  });
+  return positionFilter;
+}
 
 //Progression 5 - Filter players that have won ______ award
 
+function filterByAward(awardName){
+  var awardFilter = players.filter((player) =>{
+    return player.awards.name===awardName;
+  });
+  return awardFilter;
+}
+
 //Progression 6 - Filter players that won ______ award ____ times
+
+function filterByAwardxTimes(awardName,noOfTimes){
+  return players.filter((p) =>{
+    var awardCount = players.awards.filter(award => award.name===awardName).length;
+    return awardCount===noOfTimes;
+  });
+};
 
 //Progression 7 - Filter players that won ______ award and belong to ______ country
 
+function filterByAwardxCountry(awardName,country){
+  return players.filter(player => {
+    var hasAward = players.awards.some(award => award.name===awardName);
+    return hasAward && player.country===country;
+  });
+};
+
 //Progression 8 - Filter players that won atleast ______ awards, belong to ______ team and are younger than ____
 
+function filterByNoOfAwardsxTeamxAge(noOfAwards,team,age){
+  return players.filter(player =>{
+    var awardsCount = player.awards.length;
+    return(awardsCount>=noOfAwards && player.team===team && player.age===age);
+  });
+};
+
 //Progression 9 - Sort players in descending order of their age
+
+function SortByAge(){
+  return player.slice().sort((a,b)=>{
+    if (a.age<b.age){
+      return 1;
+    }
+    else if(b.age<a.age){
+      return -1;
+    }
+    else{
+      return 0;
+    }
+  });
+};
 
 //Progression 10 - Sort players beloging to _____ team in descending order of awards won
 
